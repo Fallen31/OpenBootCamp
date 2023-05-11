4db55f2342a62248607eb4932a8dfc486b268273
Fallen31 <36607762+Fallen31@users.noreply.github.com>
2023-05-11 16:03:39 -0600

    Ejercicio 3

---
+++ Ejercicio3.py
@@ -0,0 +1,5 @@
+peso = float(input("porfavor ingresa tu peso en KG:"))
+altura = float(input("Porfavor ingresa tu estatura en metros:")) 
+índice = peso / altura
+print(f"tu índice de masa corporal es [índice] "
+      
